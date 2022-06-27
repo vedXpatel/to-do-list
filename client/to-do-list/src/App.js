1c0e30c422ddcba7ba3b075e363c5cd39eddba98
vedXpatel <ved.gp@somaiya.edu>
2022-06-27 12:34:26 +0530

    add task page done

--- client/to-do-list/src/App.js
+++ client/to-do-list/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Task from './components/Task';
 
 
 function App() {
   return(
-    <Task/>
+    <Router>
+      <Routes>
+        <Route path="/addtask" element={<Task/>} />
+      </Routes>
+    </Router>
+
+    
   )
 }
 
--- client/to-do-list/src/components/Task.jsx
+++ client/to-do-list/src/components/Task.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {Alert} from 'react-bootstrap';
 import '../styles/task.scss';
 
 function Task() {
   const [task, setTask] = useState("");
+  const [show,setShow] = useState(false);
+  const [showError,setShowError] = useState(false);
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -12,14 +15,36 @@ function Task() {
   const submitTask = () => {
     const newTask = { "task": task };
     axios.post('http://localhost:3000/tasks/', newTask)
-      .then((res) => { console.log(res); })
-      .catch((err) => { console.log(err); })
+      .then((res) => { console.log(res); setShow(true); })
+      .catch((err) => { console.log(err); setShowError(true); })
   }
   return (
     <>
-        <h1>this is a trial branch</h1>
-      {/* <input type="text" className="taskInput" name="task" value={task} onChange={handleChange} />
-            <button className="btn" onClick={submitTask}>Add Task</button> */}
+
+      <a href="/" style={{color:"black",position:"relative",top:"7vh",left:"10vw"}}>
+        <h4>Go Back To All Tasks</h4>
+      </a>
+        <div class="card card--accent">
+          <h2>Add New Task</h2>
+          <label class="input">
+            <input class="input__field" value={task} type="text" placeholder=" " onChange={handleChange}/>
+          </label>
+          <div class="button-group">
+            <button className="sendButton" onClick={submitTask}>Add Task</button>
+          </div>
+        </div>
+        {
+          show && 
+              <Alert variant="success" onClose={() => setShow(false)} dismissible>
+                Task Added Successfully
+              </Alert>
+        }
+        {
+          showError && 
+          <Alert variant="danger" onClose={() => setShowError(false)} dismissible>
+              Task Not Added. We will resolve the error in few minutes.
+          </Alert>
+        }
     </>
   )
 }
